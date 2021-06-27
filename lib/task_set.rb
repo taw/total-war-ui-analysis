e995202231d86789cc9e133f96245e13d5a234f5
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2021-06-27 15:26:44 +0100

    Filter system

--- lib/task_set.rb
+++ lib/task_set.rb
@@ -1,18 +1,26 @@
 require_relative "./file_task"
+require_relative "./task_filter"
 
 class TaskSet
   attr_reader :todo
 
   def initialize(*filter)
-    @filter = filter
+    @filter = filter.map{|expr| TaskFilter.new(expr)}
     @todo = []
     data_root.find do |path|
       next if path.directory?
       @todo << FileTask.new(path)
     end
+    apply_filter!
   end
 
   def data_root
     Pathname(__dir__).parent + "data"
+  end
+
+  def apply_filter!
+    return if @filter.empty?
+    @todo.select!{|task| @filter.any?{|f| f.match?(task)} }
+    warn "No files selected" if @todo.empty?
   end
 end
